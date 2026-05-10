50a0b0c0a09464974586d587a9bfaddb649cb11f
MishtiAgrawal <mishtiagrawal2005@gmail.com>
2026-05-10 15:09:24 +0530

    Final production-ready updates: remove root duplicates, optimize file structure, prepare for deployment

--- server.js
+++ server.js
@@ -744,7 +744,6 @@ app.post('/api/help', protect, (req, res) => {
 });
 
 // SPA Support
-app.get('/lang.js', (req, res) => res.sendFile(path.join(__dirname, 'lang.js')));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
